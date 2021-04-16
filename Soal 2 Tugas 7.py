f4dafcc9c52963c9a3d9f1c1bd9d60efea0a1d2f
rizkyammar <80144906+rizkyammar@users.noreply.github.com>
2021-04-16 23:26:26 +0700

    Add files via upload

---
+++ Soal 2 Tugas 7.py
@@ -0,0 +1,46 @@
+#Program Fungsi Numerik
+print("Kali ini kita akan masuk ke program numerik")
+print()
+print("==="*12)
+print()
+
+print("Pertama Coba Program Nilai Mutlak")
+a = -159
+b = - 245
+c = 367
+print("Nilai awal a = ", a)
+print("Nilai Mutlak a adalah |a| = ", abs(a))
+print("Nilai awal b = ", b)
+print("Nilai mutlak b adalah |b| = ", abs(b))
+print("Nilai awal c = ", c)
+print("Nilai mutlak c adalah |c| = ", abs(c))
+print()
+
+print("Kali ini kita menentukan nilai maximum dan minimum ")
+a = 239469
+b = 578513
+c = 293846
+print("Nilai a = ", a)
+print("Nilai b = ", b)
+print("Nilai c = ", c)
+print("Nilai Maksimum nya adalah = ", max(a,b,c))
+print("Nilai Minimum nya adalah = ", min(a,b,c))
+print()
+
+print("Oke terakhir kita masuk program akar")
+import math
+a = 144
+b = 121
+c = 625
+print("Nilai Awal a = ", a)
+print("Nilai awal b = ", b)
+print("Nilai Awal c = ", c)
+print("Nilai Akar a = ", math.sqrt(a))
+print("Nilai akar b = ", math.sqrt(b))
+print("Nilai akar c = ", math.sqrt(c))
+print()
+
+print("Itu tadi program fungsi Numerik")
+print()
+print("Terima Kasih")
+print("==="*12)
