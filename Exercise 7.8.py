87ecfc2699c4bfdcea1f377aa97c49f6b33932c0
rizkyammar <80144906+rizkyammar@users.noreply.github.com>
2021-04-16 21:04:34 +0700

    Add files via upload

---
+++ Exercise 7.8.py
@@ -0,0 +1,68 @@
+# fungsi fungsi numerik
+
+#fungsi abs
+a = -100
+b = 125
+print("a = ", a)
+print("b = ", b)
+print("\n|a| : ", abs(a))
+print("|b| :", abs(b))
+
+#fungsi ceil
+a = 100.67
+b = 125.23
+import math
+print("a = ", a)
+print("b = ", b)
+print("\nmath a ceil: ", math.ceil(a))
+print("nilai b ceil: ", math.ceil(b))
+
+#fungsi fabs
+a = -32
+b = -3.34
+print("a = ", a)
+print("b = ", b)
+print("math.fabs a = ", math.fabs(a))
+print("math.fabs b = ", math.fabs(b))
+
+#fungsi floor
+a = 32.31
+b = 3.34
+print("a = ", a)
+print("b = ", b)
+print("math.fabs a = ", math.fabs(a))
+print("math.fabs b = ", math.fabs(b))
+
+#fungsi max() dan min()
+a = 32.31
+b = 3.34
+c = 40.22
+print("a = ", a)
+print("b = ", b)
+print("c = ", c)
+print("nilai max = ", max(a,b,c))
+print("nilai min = ", min(a,b,c))
+
+#fungsi sqrt
+a = 81
+b = 25
+c = 9
+print("a = ", a)
+print("b = ", b)
+print("c = ", c)
+print("nilai sqrt a = ", math.sqrt(a))
+print("nilai sqrt b = ", math.sqrt(b))
+print("nilai sqrt c = ", math.sqrt(c))
+
+#fungsi choice
+import random
+a = [23, 45, 98, 36]
+print("a = ", a)
+print("random 1")
+print("choice = ", random.choice(a))
+print("random 2")
+print("choice = ", random.choice(a))
+print("random 3")
+print("choice = ", random.choice(a))
+
+
