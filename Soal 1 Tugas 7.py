2f8c6261ee373979b4929f8839021a4d4812f462
rizkyammar <80144906+rizkyammar@users.noreply.github.com>
2021-04-16 23:10:01 +0700

    Add files via upload

---
+++ Soal 1 Tugas 7.py
@@ -0,0 +1,36 @@
+#menggunakan program string
+print("""Program Yang Berisi Penggunaan Program STRING
+=====================================
+""")
+str = "nah jadinya gini"
+a = str.capitalize()
+print("Yang Ini Namanya Program Capitalize")
+print(a)
+print()
+
+b = "Mari Kita Coba"
+print("Kalo yang ini namanya program upper dan lower")
+print("Upper fungsinya mengubah semua member jadi kapital")
+print("Contohnya :")
+b2 = b.upper()
+print(b2)
+print("Lower fungsinya biar jadi kecil semua")
+b = "COBA APALAGI YAAA"
+b3 = b.lower()
+print("Contohnya")
+print(b)
+print("Jadinya:")
+print(b3)
+print()
+
+print("Nah kali ini untuk mengubah member")
+b = "Mari kita coba"
+print(b)
+print("Misal mau mengubah ")
+print("Kita Ubah Mari = ", b.replace("Mari", "Ayo"))
+print()
+
+print("Nah itu tadi program string")
+print()
+print("Terima Kasih Banyaaakkk")
+print("==="*12)
